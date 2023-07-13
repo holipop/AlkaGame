b77f93e146d2c56d5b1dc3c65e36bbb78c780bb3
Sunny <adamcroslow@gmail.com>
2023-07-12 21:35:55 -0500

    playing around :3

--- CombatTest/Test/Button.cs
+++ CombatTest/Test/Button.cs
@@ -3,13 +3,10 @@ using System;
 
 public partial class Button : Godot.Button
 {
-	// Called when the node enters the scene tree for the first time.
+	[Signal]
+	public delegate void TestSignalEventHandler();
 	public override void _Ready()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		TestSignal += () => GD.Print("Emitting Test Signal!");
 	}
 }
--- CombatTest/Test/Counter.cs
+++ CombatTest/Test/Counter.cs
@@ -3,19 +3,28 @@ using System;
 
 public partial class Counter : Label
 {
+
+	private Button button;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		ConnectTestSignal();
 	}
 
 	public void OnButtonPressed()
 	{
 		int num = System.Convert.ToInt32(Text) + 1;
 		Text = num + "";
+	}
+	
+	public void ConnectTestSignal()
+	{
+		button = GetNode<Button>("../Button");
+		button.Connect("TestSignal", new Callable(this, "OnTestSignal"));
+		button.EmitSignal("TestSignal");
+	}
+	public void OnTestSignal()
+	{
+		GD.Print("Recieved Test Signal!");
 	}
 }
